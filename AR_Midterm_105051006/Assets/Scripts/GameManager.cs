a322fd480750cb6741a818c10f8e9c8d701a0d55
hk147159 <71679844+hk147159@users.noreply.github.com>
2020-11-10 15:28:23 +0800

    完成發布

---
+++ AR_Midterm_105051006/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{   
+    [Header("黑毛")]
+   public Transform B;
+    [Header("黃毛")]
+    public Transform Y;
+    [Header("黑毛動畫元件")]
+    public Animator aniB;
+    [Header("黃毛動畫元件")]
+    public Animator aniY;
+
+
+    public void Attack()
+    {
+        print("攻擊");
+        aniB.SetTrigger("攻擊觸發");
+        aniY.SetTrigger("攻擊觸發");
+    }
+
+    public void PlayAnimation(string aniName)
+    {
+        print(aniName);
+        aniB.SetTrigger(aniName);
+        aniY.SetTrigger(aniName);
+
+
+
+
+    }
+
+
+
+}
